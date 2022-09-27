13ce1fa2ad053e5c8813bd5e68cee3ef171140d1
RafoBunyatyan <rafobuniatyan@mail.ru>
2022-09-27 19:46:47 +0400

    Using The Token For Requests To Protected Resources

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,48 @@
+import { useRef, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
+	const newPasswordInputRef = useRef()
+	const authCtx = useContext(AuthContext)
+
+	const submitHandler = (event) => {
+		event.preventDefault()
+
+		const enteredNewPassword = newPasswordInputRef.current.value
+
+		// add validation
+
+		fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyBBMJ6o3AyqcZQm5Fz5vhc4rzvgqqklWz4&token=abc', {
+			method: 'POST',
+			body: JSON.stringify({
+				idToken: authCtx.token,
+				password: enteredNewPassword,
+				returnSecureToken: false
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+		}).then(res => {
+			// assumption: Always succeeds!
+
+
+		})
+	}
+
+
+	return (
+		<form className={classes.form} onSubmit={submitHandler}>
+			<div className={classes.control}>
+				<label htmlFor='new-password'>New Password</label>
+				<input type='password' id='new-password' minLength='7' ref={newPasswordInputRef} />
+			</div>
+			<div className={classes.action}>
+				<button>Change Password</button>
+			</div>
+		</form>
+	);
 }
 
 export default ProfileForm;
